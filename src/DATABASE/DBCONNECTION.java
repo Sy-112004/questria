3fd7ca42016e148caa5b6cfa04f87d30f2ac4697
sav1-1 <TIFFANYSAVANNAHFSEE@GMAIL.COM>
2025-11-05 13:22:02 +0800

    Inayos mga mali ni Sy - Dave

--- src/DATABASE/DBCONNECTION.java
+++ src/DATABASE/DBCONNECTION.java
@@ -1,9 +1,9 @@
-package DATABASE;
+package database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DBCONNECTION {
+public class DbConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/questria?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
     private static final String PASSWORD = "rootMySQL1234";
--- src/DATABASE/DBtest.java
+++ src/DATABASE/DBtest.java
@@ -1,9 +1,9 @@
-package DATABASE;
+package database;
 import java.sql.Connection;
 
-public class DBtest {
+public class DbTest {
     public static void main(String [] args){
-        Connection conn = DBCONNECTION.getConnection();
+        Connection conn = DbConnection.getConnection();
         if (conn !=null){
             System.out.println("test connection is working");
         }
